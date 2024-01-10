3cf9a22082b795a383d2b50c8be4e65bd50d3914
rodrolira <rlirapizarro@gmail.com>
2024-01-10 19:40:40 -0300

    Add albums, eps, events, multimedia items, and tracks tables (generated by blackbox.ai )

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_10_034828) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_10_223424) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_10_034828) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "albums", force: :cascade do |t|
+    t.string "title"
+    t.bigint "release_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["release_id"], name: "index_albums_on_release_id"
+  end
+
   create_table "artists", force: :cascade do |t|
     t.string "name"
     t.string "profession"
@@ -47,11 +55,49 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_10_034828) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "eps", force: :cascade do |t|
+    t.string "title"
+    t.bigint "release_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["release_id"], name: "index_eps_on_release_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.datetime "date"
+    t.string "location"
+    t.text "description"
+    t.string "buy_link"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "multimedia_items", force: :cascade do |t|
+    t.string "title"
+    t.string "content_type"
+    t.string "link"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_multimedia_items_on_user_id"
+  end
+
   create_table "releases", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tracks", force: :cascade do |t|
+    t.string "title"
+    t.bigint "release_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["release_id"], name: "index_tracks_on_release_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -66,4 +112,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_10_034828) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "albums", "releases"
+  add_foreign_key "eps", "releases"
+  add_foreign_key "events", "users"
+  add_foreign_key "multimedia_items", "users"
+  add_foreign_key "tracks", "releases"
 end
